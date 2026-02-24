8f58d46f2ce93fe57e2ffe8852d1d79b7294f633
xelywn <selywnk@gmail.com>
2026-02-24 17:12:17 +0300

    Added myproject files

---
+++ myproject/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Simple E-commerce platform built with PHP, CSS Flexbox, and JavaScript.">
+    <meta name="keywords" content="Ecommerce, Online Store, PHP Shop, Shopping Cart">
+    <meta name="author" content="Your Name">
+
+    <title>Simple E-Commerce</title>
+
+  <link rel="stylesheet" href="/myproject/css/style.css">
+</head>
+<body>
+
+<header>
+    <h1>My E-Commerce Store</h1>
+</header>
