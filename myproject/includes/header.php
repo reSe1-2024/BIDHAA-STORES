1662a6742cc68e66a81e169c62d4ee6225a06d08
emmanuel@Kamande <emmanuelkamande39@gmail.com>
2026-02-25 19:06:55 +0300

    header styling

--- myproject/includes/header.php
+++ myproject/includes/header.php
@@ -14,26 +14,36 @@
 <body>
     
 <header>
+    <img src="img/shopping-cart.png" alt="bidhaa-stores logo" class="logo" width="50" height="50">
     <h1>BIDHAA-STORES</h1>
 <nav>
 <?php if(isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
-    <a href="./index.php">Home</a>
-    <a href="./checkout.php">Checkout</a>
-    <a href="?logout=true">Logout</a>
-    <a href="./admin.php">Admin</a>
+    <ul>
+    <li class="nav-item"><a href="./index.php">Home</a></li >
+    <li class="nav-item"><a href="./checkout.php">Checkout</a></li >
+    <li class="nav-item"><a href="?logout=true">Logout</a></li >
+    <li class="nav-item"><a href="./admin.php">Admin</a></li >
+    </ul>
+   
 
 <?php elseif(isset($_SESSION['user_id'])): ?>
-    <a href="./index.php">Home</a>
-    <a href="./checkout.php">Checkout</a>
-    <a href="?logout=true">Logout</a>
+    <ul>
+    <li class="nav-item"><a href="./index.php">Home</a></li >
+    <li class="nav-item"><a href="./checkout.php">Checkout</a></li >
+    <li class="nav-item"><a href="?logout=true">Logout</a></li >
+    </ul>
+    
 
 <?php else: ?>
-    <a href="./index.php">Home</a>
-    <a href="./login.php">Login</a>
-    <a href="./register.php">Register</a>
+    <ul>
+    <li class="nav-item"><a href="./index.php">Home</a></li >
+    <li class="nav-item"><a href="./login.php">Login</a></li >
+    <li class="nav-item"><a href="./register.php">Register</a></li >
+    </ul>
+    
 
 <?php endif; ?>
 
-<button id="theme-toggle">Toggle Theme</button>
+<button id="theme-toggle" class="toggle-btn">Toggle Theme</button>
 </nav><hr>
 </header>
